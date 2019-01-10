dfa56b80d4b94956095450b302991de367dc3a1f
Dylan Van Assche <dylan.van.assche@protonmail.com>
2019-01-10 16:09:40 +0100

    Download data at startup if needed + departureTime dispatchers

--- connections.py
+++ connections.py
@@ -1,9 +1,37 @@
 #!/usr/bin/python3
 
 import cherrypy
-
+import dateutil.parser
+import json
+import os
 
 class Connections(object):
     @cherrypy.expose
-    def index(self):
-        return "List of connections"
+    @cherrypy.tools.json_out()
+    def index(self, departureTime):
+        target_date = dateutil.parser.parse(departureTime)
+        target_file = None
+
+        files = []
+        for f in os.listdir("data"):
+            path = os.path.join("data", f)
+            if os.path.isfile(path):
+                files.append(path)
+        files.sort()
+
+        for i in range(0, len(files)-1):
+            current_file = files[i]
+            next_file = files[i+1]
+            current_date = dateutil.parser.parse(os.path.basename(os.path.splitext(current_file)[0]))
+            next_date = dateutil.parser.parse(os.path.basename(os.path.splitext(next_file)[0]))
+
+            if current_date <= target_date < next_date:
+                print("Target date: {0}, between files: {1} and {2}".format(departureTime, current_file, next_file))
+                target_file = current_file
+                break
+
+        with open(target_file, "r") as json_file:
+            json_data = json.load(json_file)
+
+        # Return JSON data
+        return json_data
--- main.py
+++ main.py
@@ -1,14 +1,50 @@
 #!/usr/bin/python3
 
 import cherrypy
+import requests
+import os
+import dateutil.parser
+import json
+from urllib.parse import urlparse, parse_qs
+
 from agency import Agency
 from connections import Connections
 from events import Events
 
+URL = "https://graph.irail.be/sncb/connections?departureTime=2019-01-10T00:00:00.000Z"
+STOP_TIME = "2019-01-11T00:00:00.000Z"
 
 class LCServer(object):
     def __init__(self):
         self.sncb = SNCB()
+
+        # Download a full day of Linked Connections data if needed
+        if not os.path.isdir("data"):
+            os.mkdir("data")
+            self.fetch_fragments()
+
+        else:
+            # Loop through each fragment and see if they are all there
+            pass
+
+    def fetch_fragments(self):
+        url = URL
+
+        while True:
+            print("Downloading: " + url)
+            fragment = requests.get(url).json()
+
+            # Save fragment
+            departure_time_query = parse_qs(urlparse(url).query)["departureTime"][0];
+            with open("data/" + departure_time_query + ".jsonld", "w") as json_file:
+                json.dump(fragment, json_file)
+
+            # Find next fragment
+            url = fragment["hydra:next"]
+            departure_time_query = parse_qs(urlparse(url).query)["departureTime"][0];
+            date = dateutil.parser.parse(departure_time_query)
+            if date >= dateutil.parser.parse(STOP_TIME):
+                break
 
     @cherrypy.expose
     def index(self):
