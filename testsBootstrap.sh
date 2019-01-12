9e340ddce12059814a005893d9a223612b44febc
Dylan Van Assche <dylan.van.assche@protonmail.com>
2019-01-12 10:53:38 +0100

    Start test after server is ready

--- testsBootstrap.sh
+++ testsBootstrap.sh
@@ -6,7 +6,17 @@ python main.py &
 cd ..
 
 # Wait until all connections and events are generated
-sleep 600s
+while [ 1 ]
+do
+	sleep 10s
+	ping -c 3 127.0.0.1:8080 > /dev/null 2>&1
+	if [ $? -eq 0 ]
+	then
+		echo "Server is available now!"
+		break
+	fi
+done
+sleep 5s
 
 # Run tests
 python tests/tests.py
